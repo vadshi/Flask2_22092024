2f48c8fb2e29a1a2fe22d8c8334cd761ba9dfe01
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-09-22 12:46:23 +0300

    Add models and handlers

--- First/my_app/__init__.py
+++ First/my_app/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
+from my_app.hello_as_bp.handlers import hello
+
 
 app = Flask(__name__)
+app.register_blueprint(hello)
---
+++ First/my_app/hello_as_bp/handlers.py
@@ -0,0 +1,16 @@
+from my_app.hello_as_bp.models import MESSAGES
+from flask import Blueprint, jsonify
+
+
+hello = Blueprint('hello', __name__)
+
+
+@hello.route("/")
+@hello.route("/hello", methods=["GET"])
+def hello_world():
+    return jsonify(MESSAGES['default']), 200
+
+
+@hello.get("/show/<key>")
+def get_message(key: str):
+    return MESSAGES.get(key) or f'URL: "/show/{key}" not found'
