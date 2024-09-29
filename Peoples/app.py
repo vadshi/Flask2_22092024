b3b7c11153f332c7327381275f04287d5f7430be
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-09-29 11:40:02 +0300

    Add day1 HW solution

--- Peoples/app.py
+++ Peoples/app.py
@@ -7,7 +7,7 @@
 Создать по 10 строк в каждом файле.
 """
 import sys
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
 from faker import Faker
 
 app = Flask(__name__)
@@ -42,6 +42,56 @@ def get_names():
         for raw_line in f:
             names.append(raw_line.strip())
     return render_template("names.html", people_names=names)  # {"people_names": names}
+
+
+@app.route('/table')
+def get_table():
+    datas = []
+    with open('./files/humans.txt', encoding='utf-8') as f:
+        keys = ('surname', 'name', 'middle_name')
+        for raw_line in f:
+            data = raw_line.strip().split(',')       
+            person = dict(zip(keys, data))
+            datas.append(person)
+    return render_template('table.html', people_datas=datas)
+
+
+@app.route('/users')
+def get_users_list():
+    datas = []
+    with open('./files/users.txt', encoding='utf-8') as f:
+        keys = ('login', 'fio', 'sex', 'address', 'email', 'birth_date')
+        for raw_line in f:
+            data = raw_line.strip().split(';')
+            datas.append(dict(zip(keys, data)))
+        
+    return render_template('users_list.html', users_datas=datas)
+
+
+@app.route("/users/<login>")
+def get_user_info(login: str):
+    element = None
+    with open('./files/users.txt', encoding='utf-8') as f:
+        for raw_line in f:
+            data = raw_line.strip().split(';')
+            if data[0] == login:
+                fio = data[1].split()
+                surname = fio[0]
+                name = fio[2]
+                middle_name = fio[1]
+                birth_date = data[5].split('-')     # делаю красивую дату
+                birth_date.reverse()                # делаю красивую дату
+                birth_date = '.'.join(birth_date)   # делаю красивую дату
+                element = {
+                    'login': data[0], 'surname': surname,
+                    'name': name, 'middle_name': middle_name,
+                    'birth_date': birth_date, 'email': data[4],
+                    'phone': fake.phone_number()
+                     }
+                break
+    if element is None:
+        abort(404)
+    return render_template('user_info.html', user_info=element)
 
 
 if __name__ == '__main__':
