fe464aa2625d42ca341454d6990dece64e9e7b72
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-09-29 13:01:56 +0300

    Add author and quote models

---
+++ QuoteAPI/api/models/quote.py
@@ -0,0 +1,31 @@
+from api import db
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey
+from api.models.author import AuthorModel  # type: ignore
+
+
+class QuoteModel(db.Model):
+    __tablename__ = 'quotes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author_id: Mapped[str] = mapped_column(ForeignKey('authors.id'))
+    author: Mapped['AuthorModel'] = relationship(back_populates='quotes')
+    text: Mapped[str] = mapped_column(String(255))
+    rating: Mapped[int] = mapped_column(server_default='1')
+
+    def __init__(self, author: AuthorModel, text, rating):
+        self.author_id = author.id
+        self.text = text
+        self.rating = rating
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "author": self.author_id,
+            "text": self.text,
+            "rating": self.rating
+        }
+    
+    def __repr__(self) -> str:
+        return f'Quote({self.text})'
