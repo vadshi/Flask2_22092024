481390c040fb94714bc06a993af6d556474bddd6
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-06 10:35:47 +0300

    Add surname field and author's handlers

--- QuoteAPI/api/handlers/author.py
+++ QuoteAPI/api/handlers/author.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from api import app, db
 from api.models.author import AuthorModel # type: ignore
 from flask import abort, jsonify, request
@@ -12,17 +13,16 @@ def get_authors():
     return jsonify(authors), 200
 
 
-@app.route("/authors", methods=['POST'])
+@app.route('/authors', methods=['POST'])
 def create_author():
     author_data = request.json
-    author = AuthorModel(author_data.get('name', "Petr"))
+    author = AuthorModel(**author_data)
     db.session.add(author)
     try:
         db.session.commit()
     except Exception as e:
         db.session.rollback()
-        abort(503, f"Database error: {str(e)}")
-    # instance -> dict -> json
+        abort(503, f'Database error: {str(e)}')
     return jsonify(author.to_dict()), 201
                    
         
@@ -31,3 +31,34 @@ def get_author_by_id(author_id: int):
     author = db.get_or_404(AuthorModel, author_id)
     # instance -> dict -> json
     return jsonify(author.to_dict()), 200   
+
+@app.route("/authors/<int:author_id>", methods=["PUT"])
+def edit_author(author_id):
+    author: AuthorModel = db.get_or_404(AuthorModel, author_id)
+
+    data: dict = request.json
+
+    if len(data) == 0:
+        return "No valid data to update", HTTPStatus.BAD_REQUEST
+
+    try:
+        for key, value in data.items():
+            if not hasattr(author, key):
+                raise Exception(f"Invalid key: {key}. Valid: author")
+            setattr(author, key, value)
+        db.session.commit()
+        return author.to_dict(), 200
+    except Exception as e:
+        return str(e), HTTPStatus.BAD_REQUEST
+    
+
+@app.route("/authors/<int:author_id>", methods=["DELETE"])
+def delete_author(author_id: int):
+    author = db.get_or_404(AuthorModel, author_id)
+    db.session.delete(author)
+    try:
+        db.session.commit()
+        return f"Author with id {author_id} deleted"
+    except Exception as e:
+        db.session.rollback()
+        abort(503, f"Database error: {str(e)}")    
--- QuoteAPI/api/models/author.py
+++ QuoteAPI/api/models/author.py
@@ -1,5 +1,5 @@
 from api import db
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship, WriteOnlyMapped
 from sqlalchemy import String
 
 
@@ -7,14 +7,18 @@ class AuthorModel(db.Model):
     __tablename__ = 'authors'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[int] = mapped_column(String(32), index= True, unique=True)
-    quotes: Mapped[list['QuoteModel']] = relationship(back_populates='author', lazy='dynamic')
+    name: Mapped[int] = mapped_column(String(32), index=True, unique=True)
+    # default -> for new instance, server_default -> for instances that already exist in table
+    surname: Mapped[str] = mapped_column(String(32), index=True, default='Petrov', server_default="Smirnov")
+    quotes: Mapped[WriteOnlyMapped] = relationship(back_populates='author')
 
-    def __init__(self, name):
+    def __init__(self, name, surname):
         self.name = name
+        self.surname = surname
 
     def to_dict(self):
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'surname': self.surname,
             }
