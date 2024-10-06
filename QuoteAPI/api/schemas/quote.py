e120bb5dcc64b2c0830369c67d4594dde38a97a7
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-06 15:21:42 +0300

    Add custom validator

--- QuoteAPI/api/schemas/quote.py
+++ QuoteAPI/api/schemas/quote.py
@@ -1,14 +1,23 @@
 from api import ma
+from marshmallow import EXCLUDE
 from api.models.quote import QuoteModel
 from api.schemas.author import AuthorSchema
+
+
+def rating_validate(value: int):
+    # if return False -> raise ValidationError
+    return value in range(1, 6)
+
 
 class QuoteSchema(ma.SQLAlchemySchema):
     class Meta:
         model = QuoteModel
+        unknown = EXCLUDE
 
     id = ma.auto_field()
     text = ma.auto_field()
     author = ma.Nested(AuthorSchema())
+    rating = ma.Integer(strict=True, validate=rating_validate)
 
 
 quote_schema = QuoteSchema()
