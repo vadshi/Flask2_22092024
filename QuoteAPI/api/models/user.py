61b5d98f6e1d104ad3c6eb3a1eb4d4f579be99b8
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-10-13 15:21:02 +0300

    Add form example

--- QuoteAPI/api/models/user.py
+++ QuoteAPI/api/models/user.py
@@ -1,10 +1,7 @@
 from passlib.apps import custom_app_context as pwd_context
 from api import app, db
-from config import Config
 import sqlalchemy.orm as so
 import sqlalchemy as sa
-# from itsdangerous import URLSafeSerializer
-# from itsdangerous import BadSignature
 import jwt
 from time import time
 
@@ -27,25 +24,15 @@ class UserModel(db.Model):
         return pwd_context.verify(password, self.password_hash)
     
     def generate_auth_token(self):
-        # s = URLSafeSerializer(Config.SECRET_KEY)
-        # return s.dumps({'id': self.id})
         token = jwt.encode({'id': self.id, "exp": int(time() + 3600)},
                            key=app.config['SECRET_KEY'], algorithm="HS256")
         return token
 
     @staticmethod
     def verify_auth_token(token):
-        print(f'{token = }')
-        # s = URLSafeSerializer(Config.SECRET_KEY)
-        # try:
-        #     data = s.loads(token)
-        # except BadSignature:
-        #     return None  # invalid token
         try:
             data = jwt.decode(token, key=app.config['SECRET_KEY'], algorithms=["HS256"])
-            print(f'{data = }')
         except Exception as e:
-            print(str(e))
             return None # invalid token
         user = db.get_or_404(UserModel, data['id'])
         return user
---
+++ QuoteAPI/examples/form_example.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+
+app = Flask(__name__)
+app.secret_key = "secret key example"
+
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired(), Length(min=3, max=10)])
+
+
+@app.route("/submit", methods=["GET", "POST"])
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect(url_for("done"))
+    return render_template("index.html", form=form)
+
+
+@app.route("/success")
+def done():
+    return "Well done"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
